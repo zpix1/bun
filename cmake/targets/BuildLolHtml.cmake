8ba0f2080a651de6548dd674f7d3f409e212e581
zpix1 <zpix1@users.noreply.github.com>
2026-04-05 03:56:17 +0000

    deps: update lolhtml to v2.7.2 (e3aa54798602dd27250fafde1b5a66f080046252)

--- cmake/targets/BuildLolHtml.cmake
+++ cmake/targets/BuildLolHtml.cmake
@@ -4,7 +4,7 @@ register_repository(
   REPOSITORY
     cloudflare/lol-html
   COMMIT
-    4f8becea13a0021c8b71abd2dcc5899384973b66
+    e3aa54798602dd27250fafde1b5a66f080046252
 )
 
 set(LOLHTML_CWD ${VENDOR_PATH}/lolhtml/c-api)
